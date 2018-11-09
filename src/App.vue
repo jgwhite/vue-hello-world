a51e01b1758ec550e5602a75e13aba0bdb1639be
Jamie White <jamie@jgwhite.co.uk>
2018-11-09 12:48:28 +0000

    Tweaks

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <h1>Hello, World!</h1>
 </template>
 
 <script>
